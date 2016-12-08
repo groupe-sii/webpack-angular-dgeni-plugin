65ec1451b3a90d1bc52f9706c270400fd4b69f65
Olivier LE LAY <liollury@gmail.com>
2016-12-08 11:17:00 +0100

    feat(plugin): first commit, add base templating and webpack plugin structure

---
+++ index.js
@@ -0,0 +1,49 @@
+const Dgeni = require('dgeni'),
+    copy = require('copy');
+
+function DocsGeneratorPlugin (options) {
+    this.options = Object.assign({
+        enable       : true,
+        staticContent: './docs',
+        sources      : [
+            {
+                include : 'src/app/**/**/*.js',
+                basePath: 'src/app'
+            }
+        ],
+        output       : 'dist-docs'
+    }, options);
+
+    if (!Array.isArray(this.options.sources)) {
+        this.options.sources = [].concat(this.options.sources);
+    }
+
+    this.options.sources.push({
+        include   : `${this.options.staticContent}/**/*.ngdoc`,
+        basePath  : this.options.staticContent,
+        fileReader: 'ngdocFileReader'
+    });
+
+    this.dgeni = new Dgeni([require('./config')(this.options.output, this.options.sources)])
+}
+
+DocsGeneratorPlugin.prototype.apply = function (compiler) {
+    let options = this.options;
+    let dgeni = this.dgeni;
+
+    compiler.plugin("compile", function () {
+        if (options.enable) {
+            dgeni.generate();
+            copy('./tools/docs-generator/app/**/*.{js,html,css}', options.output, (err) => {
+                if (err) throw err;
+            });
+        }
+    });
+
+
+    compiler.plugin("emit", (compilation, callback) => {
+        callback();
+    });
+};
+
+module.exports = DocsGeneratorPlugin;
